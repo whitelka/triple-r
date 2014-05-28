c47176dd51b803aca42f8aa026b80b136fb8c26e
kyunga <kyunga@seedtime.co>
2014-05-29 00:01:06 +0900

    modify: comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,7 @@
 class CommentsController < ApplicationController
-  before_action :set_comment, only: [:show, :edit, :update, :destroy, :like]
-
   def index
     @comments = Comment.all.page(params[:page]).order('created_at DESC').per(5)
     @comment = Comment.new
-  end
-
-  def update
-    @comment.like += 1
-    @comment.save
-    @comments = Comment.all.page(params[:page]).order('created_at DESC').per(5)
-    redirect_to root_path(page: params[:page])
   end
 
   def create
@@ -23,14 +14,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def destroy
-    # @comment.destroy
-    # respond_to do |format|
-    #   format.html { redirect_to comments_url }
-    #   format.json { head :no_content }
-    # end
-  end
-
   def like
     @comment = Comment.find(params[:id])
     @comment.like += 1
@@ -40,11 +23,6 @@ class CommentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
       params.require(:comment).permit(:data, :location)
