d68436bc0ce12fec5d942be2c7d0533b97f2cfb7
kyunga <kyunga@seedtime.co>
2014-05-28 17:00:49 +0900

    modify dash file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,14 @@ class CommentsController < ApplicationController
   before_action :set_comment, only: [:show, :edit, :update, :destroy]
 
   def index
-    @comments = Comment.all.page(params[:page]).order('created_at DESC').per(2)
+    @comments = Comment.all.page(params[:page]).order('created_at DESC').per(5)
     @comment = Comment.new
   end
 
   def update
     @comment.like += 1
     @comment.save
-    @comments = Comment.all.page(params[:page]).order('created_at DESC').per(2)
+    @comments = Comment.all.page(params[:page]).order('created_at DESC').per(5)
     redirect_to root_path(page: params[:page])
   end
   def create
